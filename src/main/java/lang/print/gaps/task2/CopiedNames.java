80466479388b63797b8df4c9812577277dc2199a
SAmen <samenuatkhan@gmail.com>
2023-04-27 12:22:01 +0600

    done

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -3,12 +3,9 @@ package lang.print.gaps.task2;
 public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
-        variable = 2000;
         int value = 1000;
-        variable = 4000;
         int value2 = 3000;
         int variables = 5000;
         variables = 7000;
-        value = 90000;
     }
 }
